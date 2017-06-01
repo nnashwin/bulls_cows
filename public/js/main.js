ac816beb325c37c159e65f07e283e91cac3c0139
ttymed <tyler.boright@gmail.com>
2017-06-01 11:19:50 +0800

    adds game states and hides the game form when the players are logging in

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,14 @@
 	}
 
 	ready(() => {
+		const gameStates = ['login', 'play', 'gameOver'];
+		const playerInfo = [];
+		let currentState = gameStates[0];
+
 		const loginForm = document.getElementById('login-form');
+		const gameForm = document.getElementById('game-form');
+		if (currentState === 'login') {
+			gameForm.style.display = 'none';
+		}
 	});
 })();
