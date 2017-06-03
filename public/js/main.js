af7758d700f15049ee35a10bd566fe394862bb89
ttymed <tyler.boright@gmail.com>
2017-06-03 13:25:18 +0800

    abstracts displaying login screen login into a function which is called at the beginning of the game and when the loginForm is submitted

--- public/js/main.js
+++ public/js/main.js
@@ -7,41 +7,6 @@
 		}
 	}
 
-	function checkState (currentState, currentPlayer) {
-		const loginForm = document.getElementById('login-form');
-		const gameForm = document.getElementById('game-form');
-		
-
-		let currentState = gameStates[gameState];
-
-		if (currentState === 'login1' || currentState === 'login2') {
-			gameForm.style.display = 'none';
-
-			const playerNumber = document.getElementById('player-number')
-			playerNumber.innerHTML = `The current player is Player ${currentPlayer}, please enter your nickname and passcode`;
-			
-		} else if (currentState === 'game') {
-			loginForm.style.display = 'none';
-		}
-
-		loginForm.addEventListener('submit', () => {
-			const playerObj = {};
-			
-		
-			// convert array-like object to array
-			let inputs = Array.prototype.slice.call(loginForm.getElementsByTagName('input'));
-
-			inputs.map((input) => {
-				const keyStr = input.id.split('-')[0];
-				playerObj[keyStr] = input.value
-			});
-			playerInfo[`currentPlayer${currentPlayer}`] = playerObj;
-			currentPlayer = (currentPlayer % 2) + 1;
-			gameState += 1;
-			currentState = gameStates[gameState];
-		});
-	}
-
 	function clearFormInputs (formEl) {
 		var formElInputArray = Array.prototype.slice.call(formEl.getElementsByTagName('input'));
 		return formElInputArray.map((input) => {
@@ -51,6 +16,13 @@
 
 	function calcBucksAndNannies (inputStr, playerStr) {
 		
+	}
+
+	function showLoginScreen (currentPlayer, gameForm) {
+		gameForm.style.display = 'none';
+
+		const playerNumber = document.getElementById('player-number')
+		playerNumber.innerHTML = `The current player is Player ${currentPlayer}, please enter your nickname and passcode`;
 	}
 
 	function onReady () {
@@ -64,10 +36,7 @@
 
 		let currentState = gameStates[gameState];
 
-		gameForm.style.display = 'none';
-
-		const playerNumber = document.getElementById('player-number')
-		playerNumber.innerHTML = `The current player is Player ${currentPlayer}, please enter your nickname and passcode`;
+		showLoginScreen(currentPlayer, gameForm);
 			
 
 		loginForm.addEventListener('submit', () => {
@@ -79,24 +48,23 @@
 				const keyStr = input.id.split('-')[0];
 				playerObj[keyStr] = input.value
 			});
+
 			playerInfo[`Player${currentPlayer}`] = playerObj;
+
 			gameState += 1;
 			currentState = gameStates[gameState];
 
-			console.log(currentState);
 			if (currentState === 'play') {
 				gameForm.style.display = 'block';
 				loginForm.style.display = 'none';
 			} else if (currentState !== 'play') {
 				clearFormInputs(loginForm);
 				currentPlayer = (currentPlayer % 2) + 1
-				playerNumber.innerHTML = `The current player is Player ${currentPlayer}, please enter your nickname and passcode`; 
+				showLoginScreen(currentPlayer, gameForm);
 			}
-
 		});
 
 		gameForm.addEventListener('submit', () => {
-			console.log('submit game form')
 			gameState += 1;
 			currentPlayer = (currentPlayer % 2) + 1;
 			currentState = gameStates[gameState];
